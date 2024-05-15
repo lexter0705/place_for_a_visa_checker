70559a14715a78c8ea34a93866ff6438784262f5
Alexey <88457801+lexter0705@users.noreply.github.com>
2024-05-15 08:21:07 +0300

    refactor for champ

--- json_checker.py
+++ json_checker.py
@@ -2,21 +2,21 @@ import json
 
 
 def get_data_for_web_bot():
-    with open("../settings/web_bot.json", encoding="utf-8", mode="r") as read_file:
+    with open("./settings/web_bot.json", encoding="utf-8", mode="r") as read_file:
         data = json.load(read_file)
 
     return data
 
 
 def get_data_for_sender_telegram_bot():
-    with open("../settings/sender_bot.json", encoding="utf-8", mode="r") as read_file:
+    with open("./settings/sender_bot.json", encoding="utf-8", mode="r") as read_file:
         data = json.load(read_file)
 
     return data
 
 
 def get_data_for_telegram_bot():
-    with open("../settings/telegram_bot.json", encoding="utf-8", mode="r") as read_file:
+    with open("./settings/telegram_bot.json", encoding="utf-8", mode="r") as read_file:
         data = json.load(read_file)
 
     return data
